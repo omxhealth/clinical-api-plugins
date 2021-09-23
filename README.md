aa997ec41cadcd24732fa1067538b5d12fe0717b
David Ackerman <david@drugbank.com>
2021-09-23 14:21:18 -0600

    basic sample app

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # medication-search-plugin
-An example of how to use DrugBank's medication search plugin
+
+This repo provides an example of how to use DrugBank's medication search 
+plugin in a real project.
+
+We are writing this in [ruby](https://www.ruby-lang.org/en/), with the 
+[sinatra](http://sinatrarb.com) framework, but you can use whatever 
+language/framework you like.
+
+## Getting started
+
+[Docker](https://www.docker.com/get-started) is the easiest way to get
+this sample application up and running. If you want to set it up without
+Docker, the Dockerfile will still be a useful resource to understand the
+required software.
+
+If you have Docker installed, you will just need to do some minimal
+configuration. Go to the root of your project and run:
+
+```bash
+cp config.yml.example config.yml
+```
+
+Edit your config.yml to set your DrugBank API key. Then simply run:
+
+```bash
+docker-compose up -d sample-app
+```
+
+Images will be built if they haven't been built already, and a container will run
+to serve the sample application on port 8080. You can change this if you like by 
+[editing the docker-compose.yml file](https://docs.docker.com/compose/compose-file/).
+
+## Using the app
+
+Once the app is up and running, go to:
+
+[localhost:8080](http://localhost:8080) in your web browser, and you should see
+a screen like this:
+
+![not-logged-in](docs/images/not-logged-in.png)
+
+Clicking the "here" link will log you in as user 345, and you should see the first
+lookup, where we have a preset value now shows a medication ("Acetaminophen [Tylenol]
+160 mg Oral Powder"), while the other two are blank.
+
+![logged-in](docs/images/logged-in.png)
+
+Enter one or two more medications or change the existing medication if you like, and
+click submit.
+
+## More information
+
+There is more detailed information on options available when using the medication search
+widget in the wiki for this repository.
---
+++ sample-app/app.rb
@@ -0,0 +1,78 @@
+require 'yaml'
+require 'uri'
+require 'net/http'
+
+LOGGED_IN_IDS = []
+
+class SampleApp < Sinatra::Base
+  CONFIG = YAML.load(File.read(File.join(__dir__, 'config.yml'))).transform_keys(&:to_sym)
+
+  enable :sessions
+
+  helpers do
+    def logged_in?
+      LOGGED_IN_IDS.include?(session[:user_id])
+    end
+  end
+
+  get "/refresh-jwt" do
+    refresh_jwt
+  end
+
+  get "/" do
+    erb :sample
+  end
+
+  post "/" do
+    @name = @params['name']
+    @medications = [
+      @params['medication1'],
+      @params['medication2'],
+      @params['medication3']
+    ].compact
+    erb :sample
+  end
+
+  # Note: This is NOT a good way of doing a cookie based login. There's no session key
+  # created or password or anything like that, and the information on who's logged in 
+  # is stored IN MEMORY!!! The purpose here is to simulate cookie based authentication.
+  # When our plugins use the refresh_url, it should be behind a an authentication scheme
+  # like this. When running this app, you will notice that the medication search will
+  # not work until you've clicked the login link.
+  get "/login/:user_id" do
+    session[:user_id] = @params[:user_id]
+    LOGGED_IN_IDS << @params[:user_id]
+    redirect back
+  end
+
+  get "/logout" do
+    LOGGED_IN_IDS.delete(session[:user_id])
+    redirect back
+  end
+
+  get "/protected-refresh-jwt" do
+    if logged_in?
+      refresh_jwt
+    else
+      "not logged in"
+    end
+  end
+  
+  def refresh_jwt
+    uri = URI("#{CONFIG[:api_server]}/v1/tokens")
+    req = Net::HTTP::Post.new(uri)
+    # TTL is in hours by default, but if we add an 'm' to our value, we request a TTL
+    # in minutes. The following will create a token that lasts 15 minutes.
+    req.body = { ttl: '15m' }.to_json
+    req['Content-Type'] = 'application/json'
+    req['Authorization'] = CONFIG[:api_key]
+    req['Cache-Control'] = 'no-cache'
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    res = http.request(req)
+    # The response from our API comes back in JSON, with a single field: token. Here
+    # we're passing that value back as the response from the server to our client-side
+    # JavaScript code.
+    JSON.parse(res.body)['token']
+  end
+end
